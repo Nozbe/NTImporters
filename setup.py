7ce04c85eeca66e27d522bf3bd759f0acadb6384
jsamelak <jarek@nozbe.com>
2021-12-01 15:08:22 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name='ntimporters',
+    version='0.1',
+    description='Set of migrators to import data from 3rd party apps to Nozbe Teams',
+    packages = find_packages(),
+)
