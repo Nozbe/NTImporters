9c812a28bccad4d2e1ec6a9c0e253fedd9900a8c
jsamelak <jarek@nozbe.com>
2021-12-01 16:21:52 +0100

    indicate dir with packages

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(
     name='ntimporters',
     version='0.1',
     description='Set of migrators to import data from 3rd party apps to Nozbe Teams',
-    packages = find_packages(),
+    packages = find_packages(where='src'),
 )
