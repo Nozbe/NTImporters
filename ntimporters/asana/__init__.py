100e19da781225184d732da485973a5706a6bc27
jsamelak <jarek@nozbe.com>
2021-12-02 14:45:40 +0100

    basic data fetching from Asana

--- ntimporters/asana/__init__.py
+++ ntimporters/asana/__init__.py
@@ -1,7 +1,13 @@
 """Asana -> Nozbe Teams importer"""
 
+from itertools import chain
 from typing import Optional
-from openapi_client import ApiClient, Configuration, apis, models
+
+import asana
+from asana.error import AsanaError
+
+import openapi_client as nt
+from openapi_client.exceptions import OpenApiException
 
 SPEC = {
     "code": "asana",
@@ -17,10 +23,38 @@ def run_import(nt_auth_token: str, auth_token: str) -> Optional[str]:
     if not auth_token:
         return "Missing 'auth_token'"
 
-    nt_client = ApiClient(configuration=Configuration(
+    nt_client = nt.ApiClient(configuration=nt.Configuration(
         host="https://api4.nozbe.com/v1/api",
         api_key={"ApiKeyAuth": nt_auth_token},
         access_token=nt_auth_token,
     ))
-    return None
+    asana_client = asana.Client.access_token(auth_token)
+    try:
+        _import_data(nt_client, asana_client)
+    except (AsanaError, OpenApiException) as exc:
+        return str(exc)
 
+
+def _import_data(nt_client: nt.ApiClient, asana_client: asana.Client):
+    """Import everything from Asana to Nozbe Teams"""
+    me = asana_client.users.me()
+    print(me)
+    for workspace in asana_client.workspaces.find_all(full_payload=True):
+        print(workspace)
+        # users = asana_client.users.find_all({"workspace": workspace["gid"]})
+        # print(users)
+        for tag in asana_client.tags.find_by_workspace(workspace["gid"]):
+            print(tag)
+        for project in asana_client.projects.find_by_workspace(workspace["gid"]):
+            print(project)
+            for section in asana_client.sections.find_by_project(project["gid"]):
+                print(section)
+            for task in chain(
+                asana_client.tasks.find_by_project(project["gid"]),
+                asana_client.tasks.find_all({"workspace": workspace["gid"], "assignee": me["gid"]}),
+            ):
+                print(task)
+                for story in asana_client.stories.find_by_task(task["gid"]):
+                    print(story)
+                for attachment in asana_client.attachments.find_by_task(task["gid"]):
+                    print(attachment)
