a71427017b98c786ecc930f1a2339349479f3af4
jsamelak <44056210+jsamelak@users.noreply.github.com>
2021-12-08 10:52:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
     name='ntimporters',
     version='0.1',
-    description='Set of migrators to import data from 3rd party apps to Nozbe Teams'
+    description='Set of migrators to import data from 3rd party apps to Nozbe Teams',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nozbe/NTImporters",
+    author="jsamelak",
+    author_email="jarek@nozbe.com",
+    license="MIT",
 )
