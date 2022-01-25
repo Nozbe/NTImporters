7efb5b38423b9c43a798c7d425e9ef235f827add
mjamroz <jamroz@chem.uw.edu.pl>
2022-01-25 11:04:41 +0100

    monday importer + api client

---
+++ src/ntimporters/monday/importer.py
@@ -0,0 +1,198 @@
+"""Monday -> Nozbe Teams importer"""
+
+from typing import Optional, Tuple
+
+import openapi_client as nt
+from dateutil.parser import isoparse
+from ntimporters.monday.monday_api import MondayClient
+from openapi_client import apis, models
+from openapi_client.exceptions import OpenApiException
+from openapi_client.model.id16_read_only import Id16ReadOnly
+from openapi_client.model.id16_read_only_nullable import Id16ReadOnlyNullable
+from openapi_client.model.timestamp_read_only import TimestampReadOnly
+from openapi_client.model_utils import ModelNormal
+
+SPEC = {
+    "code": "monday",  # codename / ID of importer
+    "name": "Monday",  # name of application
+    "url": "https://api.developer.monday.com/docs",
+    "input_fields": ("nt_auth_token", "app_key"),
+}
+
+
+def strip_readonly(model: ModelNormal):
+    """ Strip read only fields before sending to server """
+    for field in [
+        elt
+        for elt in model.attribute_map.values()
+        if hasattr(model, elt)
+        and isinstance(getattr(model, elt), (Id16ReadOnly, Id16ReadOnlyNullable, TimestampReadOnly))
+    ]:
+        del model.__dict__.get("_data_store")[field]
+    return model
+
+
+def id16():
+    """Generate random string"""
+    return 16 * "a"
+
+
+# main method called by Nozbe Teams app
+def run_import(nt_auth_token: str, app_key: str, team_id: str) -> Optional[str]:
+    """Perform import from monday to Nozbe Teams"""
+    if not nt_auth_token:
+        return "Missing 'nt_auth_token'"
+    if not app_key:
+        return "Missing 'app_key'"
+
+    try:
+        _import_data(
+            nt.ApiClient(
+                configuration=nt.Configuration(
+                    # host="http://localhost:8888/v1/api",
+                    host="https://api4.nozbe.com/v1/api",
+                    api_key={"ApiKeyAuth": nt_auth_token},
+                )
+            ),
+            MondayClient(app_key),
+            team_id,
+        )
+
+    except OpenApiException as exc:
+        return str(exc)
+    return None
+
+
+def _import_data(nt_client: nt.ApiClient, monday_client, team_id: str):
+    """Import everything from monday to Nozbe Teams"""
+
+    def _import_project(project: dict):
+        """Import monday project"""
+        project_model = models.Project(
+            id=models.Id16ReadOnly(id16()),
+            name=models.NameAllowEmpty("!!____ " + project.get("name")),
+            team_id=models.Id16(team_id),
+            author_id=models.Id16ReadOnly(id16()),
+            created_at=models.TimestampReadOnly(1),
+            last_event_at=models.TimestampReadOnly(1),
+            ended_at=models.TimestampNullable(1)
+            if project.get("state") in ("archived", "deleted")
+            else None,
+            description=project.get("description"),
+            is_open=project.get("board_kind") == "public",
+            extra="",
+        )
+        nt_project = apis.ProjectsApi(nt_client).post_project(strip_readonly(project_model)) or {}
+        if not (nt_project_id := str(nt_project.get("id"))):
+            return
+
+        _import_project_sections(
+            nt_client, monday_client, nt_project_id, project, nt_members_by_email(nt_client)
+        )
+
+    for project in monday_client.projects():
+        _import_project(project)
+
+
+def _import_project_sections(nt_client, monday_client, nt_project_id, project, nt_members: tuple):
+    """Import monday lists as project sections"""
+    nt_api_sections = apis.ProjectSectionsApi(nt_client)
+
+    def _parse_timestamp(monday_timestamp: Optional[str]) -> Optional[models.TimestampNullable]:
+        """Parses monday timestamp into NT timestamp format"""
+        if not monday_timestamp:
+            return None
+        return models.TimestampNullable(int(isoparse(monday_timestamp).timestamp() * 1000))
+
+    sections_mapping = {}
+    for section in monday_client.sections(project.get("id")):
+        if nt_section := nt_api_sections.post_project_section(
+            strip_readonly(
+                models.ProjectSection(
+                    models.Id16ReadOnly(id16()),
+                    models.Id16(nt_project_id),
+                    models.Name(section.get("title")),
+                    models.TimestampReadOnly(1),
+                    archived_at=models.TimestampReadOnly(1) if section.get("archived") else None,
+                    position=float(section.get("position") or 1.0),
+                )
+            )
+        ):
+            sections_mapping[section.get("id")] = str(nt_section.get("id"))
+    _import_tasks(
+        nt_members, nt_client, monday_client, sections_mapping, project.get("id"), nt_project_id
+    )
+
+
+# pylint: disable=too-many-arguments
+def _import_tasks(
+    nt_members: tuple, nt_client, monday_client, sections_mapping, m_project_id, nt_project_id
+):
+    """ Import tasks """
+    nt_api_tasks = apis.TasksApi(nt_client)
+    _, author_id = nt_members
+    for task in monday_client.tasks(m_project_id):
+        due_at, responsible_id = task.get("due_at"), None
+        if due_at:
+            due_at = models.TimestampNullable(due_at) if due_at else None
+            responsible_id = author_id
+        if nt_task := nt_api_tasks.post_task(
+            strip_readonly(
+                models.Task(
+                    id=models.Id16ReadOnly(id16()),
+                    name=models.Name(task.get("name")),
+                    project_id=models.ProjectId(nt_project_id),
+                    author_id=models.Id16ReadOnly(id16()),
+                    created_at=models.TimestampReadOnly(1),
+                    last_activity_at=models.TimestampReadOnly(1),
+                    project_section_id=models.Id16Nullable(sections_mapping.get(task.get("group"))),
+                    project_position=1.0,
+                    due_at=due_at,
+                    responsible_id=models.Id16Nullable(responsible_id),
+                )
+            )
+        ):
+            _import_comments(nt_client, monday_client, str(nt_task.id), task.get("id"))
+
+
+# pylint: enable=too-many-arguments
+
+
+def _import_comments(nt_client, monday_client, nt_task_id: str, tr_task_id: str):
+    """Import task-related comments"""
+    nt_api_comments = apis.CommentsApi(nt_client)
+    for comment in sorted(
+        monday_client.comments(tr_task_id),
+        key=lambda elt: isoparse(elt.get("created_at")).timestamp(),
+    ):
+        # cannot create author_id read-only field
+        nt_api_comments.post_comment(
+            strip_readonly(
+                models.Comment(
+                    id=models.Id16ReadOnly(id16()),
+                    body=comment.get("text_body"),
+                    task_id=models.Id16(nt_task_id),
+                    created_at=models.TimestampReadOnly(1),
+                    author_id=models.Id16ReadOnly(id16()),
+                    extra="",
+                )
+            )
+        )
+
+
+def nt_members_by_email(nt_client) -> Tuple[dict, str]:
+    """Map NT emails to member ids"""
+    nt_members = {
+        str(elt.user_id): str(elt.id) for elt in apis.TeamMembersApi(nt_client).get_team_members()
+    }
+    mapping = {}
+    current_user_id = None
+    for user in apis.UsersApi(nt_client).get_users():
+        if hasattr(user, "email"):
+            email = user.email
+        elif hasattr(user, "invitation_email"):
+            email = user.invitation_email
+        if bool(user.is_me):
+            current_user_id = str(user.id)
+        mapping[str(email)] = nt_members.get(str(user.id))
+    return mapping, nt_members.get(current_user_id)
---
+++ src/ntimporters/monday/monday_api.py
@@ -0,0 +1,89 @@
+""" Monday API client module """
+import json
+from datetime import datetime
+
+import requests
+
+
+class MondayClient:
+    """ Client to connect to Monday API """
+
+    api_path = "https://api.monday.com/v2"
+    limit = 300
+
+    def __init__(self, app_key):
+        self.headers = {"Authorization": app_key}
+
+    def _req(self, query) -> dict:
+        if resp := requests.get(
+            self.api_path, json={"query": f"{{ {query} }}"}, headers=self.headers
+        ):
+            return resp.json()
+        return {}
+
+    def user(self) -> dict:
+        """ Get Monday's user email """
+        return self._req("me{email}").get("data", {}).get("me", {})
+
+    def projects(self) -> list[dict]:
+        """ Get Monday boards (NT projects) """
+        return (
+            self._req(
+                f"boards(state:all limit:{self.limit}){{name,id,state,description,board_kind}}"
+            )
+            .get("data", {})
+            .get("boards", [])
+        )
+
+    def sections(self, project_id: str) -> list:
+        """Get Monday groups (NT project sections) """
+        query = f"boards (state:all ids: {project_id}) {{ groups {{ id title archived }} }}"
+        return self._req(query).get("data", {}).get("boards", [{}])[0].get("groups", [])
+
+    def tasks(self, project_id: str) -> list:
+        """ Get Monday items (NT tasks) """
+        query = f"""boards(state:all limit:{self.limit} ids:{project_id})
+        {{
+        items {{ state id group {{id}} name column_values {{ type value text title }} }}
+        }}"""
+        # ASSUMPTION: if only one date-type column then it is due_at
+        tasks = []
+        for task in self._req(query).get("data", {}).get("boards", [{}])[0].get("items", []):
+            counter, due_at = 0, None
+            for column in task.get("column_values"):
+                if column.get("type") == "date" and column.get("value"):
+                    if (counter := counter + 1) > 1:
+                        break
+                    try:
+                        dtime = json.loads(column.get("value", "{}"))
+                        due_at = int(
+                            datetime.strptime(
+                                f"{dtime.get('date')} {dtime.get('time')}", "%Y-%m-%d %H:%M:%S"
+                            ).timestamp()
+                            * 1000
+                        )
+                    except (ValueError, json.decoder.JSONDecodeError):
+                        pass
+            task.pop("column_values", None)
+            tasks.append(
+                task
+                | {
+                    "due_at": due_at if counter == 1 else None,
+                    "group": task.get("group", {}).get("id"),
+                }
+            )
+        return tasks
+
+    def comments(self, task_id: str) -> dict:
+        """ Get Monday updates (task's comments) """
+        query = f"""items (ids: {task_id}) {{
+        updates(limit:{self.limit}) {{created_at, text_body, id, creator_id}}}}
+        """
+        return self._req(query).get("data", {}).get("items", [{}])[0].get("updates", [])
+
+    def users(self) -> dict:
+        """ Get Monday users"""
+        return {
+            str(elt.get("id")): str(elt.get("email"))
+            for elt in self._req("users {id email}").get("data", {}).get("users", [])
+        }
