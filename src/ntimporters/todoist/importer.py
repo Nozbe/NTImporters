3383f52304a16f61b7bc2f29a2d4c112b0958747
Michal Jamroz <jamroz@chem.uw.edu.pl>
2022-02-14 09:48:00 +0100

    Update src/ntimporters/todoist/importer.py
    Co-authored-by: jsamelak <44056210+jsamelak@users.noreply.github.com>

--- src/ntimporters/todoist/importer.py
+++ src/ntimporters/todoist/importer.py
@@ -38,7 +38,7 @@ def run_import(nt_auth_token: str, auth_token: str, team_id: str) -> Optional[st
         _import_data(
             nt.ApiClient(
                 configuration=nt.Configuration(
-                    host="https://devapi4.nozbe.com/v1/api",
+                    host="https://api4.nozbe.com/v1/api",
                     api_key={"ApiKeyAuth": nt_auth_token},
                 )
             ),
