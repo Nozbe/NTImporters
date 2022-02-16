eb34cb290d36d83479d387531c220c1c17b1e423
mjamroz <jamroz@chem.uw.edu.pl>
2022-02-16 11:17:55 +0100

    monday uppercase, tests

--- src/ntimporters/monday/importer.py
+++ src/ntimporters/monday/importer.py
@@ -17,7 +17,7 @@ from openapi_client.exceptions import OpenApiException
 
 SPEC = {
     "code": "monday",  # codename / ID of importer
-    "name": "Monday",  # name of application
+    "name": "MONDAY",  # name of application
     "url": "https://api.developer.monday.com/docs",
     "input_fields": ("team_id", "nt_auth_token", "app_key"),
 }
