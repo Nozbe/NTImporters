8600459337fdbe9a0cad4e9e42dfca7a6a7631a0
mjamroz <jamroz@chem.uw.edu.pl>
2022-10-21 09:50:00 +0200

    cleaning

--- src/ntimporters/rate_limiting.py
+++ src/ntimporters/rate_limiting.py
@@ -9,19 +9,18 @@ class RLProxy:
     def __init__(self, proxied_object, window=15 * 60, num_requests=450, prefix="get_"):
         """Window - timeframe in seconds , num_requests = max number of wrapped_method calls"""
         now = datetime.datetime.now()
+
+        self.__proxied = proxied_object
         self._max_rate = num_requests
         self._window = window
         self._prefix = prefix
 
         self.num_requests = 0
         self.next_reset_at = now + datetime.timedelta(seconds=self._window)
-        self.__proxied = proxied_object
 
     def __getattr__(self, attr):
         def wrapped_method(*args, **kwargs):
             """Wrapped method with rate limiting"""
-            print("C", attr)
-
             if self._prefix is None or attr.startswith(self._prefix):
                 # apply rl only to class.get_* methods (as in Todoist SDK)
                 self.check_rl()
@@ -32,7 +31,6 @@ class RLProxy:
 
     def reset(self):
         """Reset counters"""
-        # print("RESET", self.num_requests)
         now = datetime.datetime.now()
         self.next_reset_at = now + datetime.timedelta(seconds=self._window)
         self.num_requests = 0
