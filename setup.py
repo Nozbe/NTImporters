25a30221f293db4e0a81f5dd40d704755dd4ca40
mjamroz <jamroz@chem.uw.edu.pl>
2024-05-10 11:05:50 +0200

    bump asana package

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     license="MIT",
     install_requires=[
         "python_dateutil",
-        "asana<=5.0.6",
+        "asana>=5.0.6",
         "todoist-api-python",
         "todoist-python",
     ],
