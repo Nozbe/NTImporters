997fb6636abaf64cd14daba4b02c8253a86d8c6b
mjamroz <jamroz@chem.uw.edu.pl>
2024-07-29 13:04:27 +0200

    bump openapi client

--- src/openapi_client/api/__init__.py
+++ src/openapi_client/api/__init__.py
@@ -1,3 +1,18 @@
-# do not import all apis into this module because that uses a lot of memory and stack frames
-# if you need the ability to import all apis from one package, import them with
-# from openapi_client.apis import CommentsApi
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.attachments_api import AttachmentsApi
+from openapi_client.api.comments_api import CommentsApi
+from openapi_client.api.group_assignments_api import GroupAssignmentsApi
+from openapi_client.api.project_accesses_api import ProjectAccessesApi
+from openapi_client.api.project_groups_api import ProjectGroupsApi
+from openapi_client.api.project_sections_api import ProjectSectionsApi
+from openapi_client.api.projects_api import ProjectsApi
+from openapi_client.api.reminders_api import RemindersApi
+from openapi_client.api.tag_assignments_api import TagAssignmentsApi
+from openapi_client.api.tags_api import TagsApi
+from openapi_client.api.tasks_api import TasksApi
+from openapi_client.api.team_members_api import TeamMembersApi
+from openapi_client.api.teams_api import TeamsApi
+from openapi_client.api.users_api import UsersApi
+
